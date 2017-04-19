798095865fb130f62d23984624d29584a8470d40
Oshan Shrestha <oshan_shrestha@outlook.com>
2017-04-19 12:07:36 +0545

    Update v0.0.2

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -12,12 +13,27 @@ namespace Vidly.Controllers
         // GET: Movies
         public ActionResult Random()
         {
-            var movie = new Movie() { Name = "Shrek" };
-            return View(movie);
+            var movie = new Movie() { Name = "Avengers" };
+
+            var customers = new List<Customer>
+            {
+                new Customer { Name= "Customer1"},
+                new Customer { Name= "Customer2"},
+                new Customer { Name= "Customer3"}
+            };
+
+            var viewModel = new RandomMovieViewModel()
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
         }
         public ActionResult Edit(int id)
         {
-            return Content("id= " + id);
+            var idText = "id: " + id;
+            return Content(idText);
         }
 
         public ActionResult Index(int? pageIndex, String sortBy)
@@ -29,6 +45,13 @@ namespace Vidly.Controllers
                 sortBy = "Name";
 
             return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleasedDate(int year, int month)
+        {
+            var text = year + "/" + month;
+            return Content(text);
         }
     }
 }
